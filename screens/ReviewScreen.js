0e343f083988813b692006520dda82bc967ca73a
shawn <shawnantonucci2018@gmail.com>
2019-04-18 18:50:50 -0700

    added a top navbar and a review screen setup

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Platform, StyleSheet } from "react-native";
+import { Button } from "react-native-elements";
 
 class ReviewScreen extends Component {
-    render() {
-        return (
-            <View>
-                <Text>ReviewScreen</Text>
-            </View>
-        );
-    }
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "Review Jobs",
+      headerRight: (
+        <Button
+          title="Settings"
+          buttonStyle={styles.button}
+          onPress={() => navigation.navigate("settings")}
+          type="clear"
+        />
+      ),
+      headerStyle: {
+        marginTop: Platform.OS === "android" ? 24 : 0
+      }
+    };
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  button: {
+    backgroundColor: "transparent",
+  }
+});
 
 export default ReviewScreen;
