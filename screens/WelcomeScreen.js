7d22a108a6bbcd8d9f0360f0c056f67544e36b55
shawn <shawnantonucci2018@gmail.com>
2019-04-18 22:55:37 -0700

    created welcome screen with scrollable text

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+    { text: "Welcome to job app", color: "#03A9F4" },
+    { text: "Use this to get a job", color: "#009688" },
+    { text: "Set your location, then swipe away", color: "#03A9F4" },
+]
 
 class WelcomeScreen extends Component {
     render() {
         return (
             <View>
-                <Text>WelcomeScreen</Text>
+                <Slides data={SLIDE_DATA} />
             </View>
         );
     }
