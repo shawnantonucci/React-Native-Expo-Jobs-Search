77119bf855d6499c337595235203e371b68a77d7
shawn <shawnantonucci2018@gmail.com>
2019-04-21 17:04:47 -0700

    hid navigation / added a map to mapscreen

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -35,6 +35,8 @@ const MainNavigator = createAppContainer(
         })
       })
     }
+  }, {
+    backBehavior: 'home'
   })
 );
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,30 +1,31 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
-import { AppLoading } from 'expo';
-import Slides from '../components/Slides';
- 
+import _ from "lodash";
+import React, { Component } from "react";
+import { View, Text, AsyncStorage } from "react-native";
+import { AppLoading } from "expo";
+import Slides from "../components/Slides";
+
 const SLIDE_DATA = [
-  { text: 'Welcome to JobApp', color: '#03A9F4' },
-  { text: 'Use this to get a job', color: '#009688' },
-  { text: 'Set your location, then swipe away', color:'#03A9F4' }
+  { text: "Welcome to JobApp", color: "#03A9F4" },
+  { text: "Use this to get a job", color: "#009688" },
+  { text: "Set your location, then swipe away", color: "#03A9F4" }
 ];
 class WelcomeScreen extends Component {
-  
-  state = { token: null }
+  state = { token: null };
+
   async componentWillMount() {
-    let token = await AsyncStorage.getItem('fb_token');
+    let token = await AsyncStorage.getItem("fb_token");
     if (token) {
-      this.props.navigation.navigate('map');
+      this.props.navigation.navigate("map");
       this.setState({ token });
-    }else {
+    } else {
       this.setState({ token: false });
     }
   }
- 
+
   onSlidesComplete = () => {
-    this.props.navigation.navigate('auth');
-  }
+    this.props.navigation.navigate("auth");
+  };
+  
   render() {
     if (_.isNull(this.state.token)) {
       return <AppLoading />;
